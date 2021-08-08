daba3742c40f6b5b56ce7cead6715ebc27a1c7d9
yushi-pipi <yu820shi@gmail.com>
2021-08-08 22:16:31 +0900

    heroku 対応２

--- get_vacancy_room.py
+++ get_vacancy_room.py
@@ -87,7 +87,8 @@ def post_message_by_line(message):
 driver_path = '/app/.chromedriver/bin/chromedriver'
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
-
+#※headlessにしている
+driver = webdriver.Chrome(options=options, executable_path=driver_path)
 
 # サイトへのアクセス
 driver.get('https://reserve.tokyodisneyresort.jp/hotel/search/')
